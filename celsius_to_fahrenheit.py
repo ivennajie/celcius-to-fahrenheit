61571e4917e04419cc42e70b3591667de45041c4
iven23 <iven23@gmail.com>
2021-12-16 20:48:14 +0700

    Add an input message.
    Add error message when string or empty value is received as an input.

--- celsius_to_fahrenheit.py
+++ celsius_to_fahrenheit.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
-celsius = int(input())
+import sys
+
+celsius = input("Enter degree in Celsius: ")
+try:
+	celsius = int(celsius)
+except ValueError:
+	print("Please enter a valid number!")
+	sys.exit(0)
 
 def conv(c):
+
 	return (9/5*c+32)
 
 fahrenheit = conv(celsius)
